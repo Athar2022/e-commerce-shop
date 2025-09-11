1e0b43c9f960b21d9460d0c457bca9e8a1ecc6af
Athar2022 <athar.hamady@gmail.com>
2025-09-12 01:00:53 +0300

    feat: Create notification for new orders

---
+++ app/Notifications/NewOrderNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewOrderNotification extends Notification
+{
+    use Queueable;
+
+    public $order;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('New Order Alert')
+                    ->line('A new order has been placed.')
+                    ->line('Order ID: ' . $this->order->id)
+                    ->line('Thank you for using our platform!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toDatabase($notifiable): array
+    {
+        return [
+            'order_id' => $this->order->id,
+            'amount' => $this->order->amount,
+            'message' => 'A new order has been placed.',
+        ];
+    }
+}
