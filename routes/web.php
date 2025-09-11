dcc915672b8c13c0bff6a81a91c28a034e45c1b0
Athar2022 <athar.hamady@gmail.com>
2025-09-12 01:31:09 +0300

    feat: Add routes to test mail and notifications

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Mail\WelcomeEmail;
+use App\Notifications\NewOrderNotification;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,16 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+//last challenge router
+Route::get('/test-communications', function () {
+    $user = User::first(); 
+
+    Mail::to($user->email)->send(new WelcomeEmail($user));
+
+    $fakeOrder = (object) ['id' => 101, 'amount' => 250];
+    $user->notify(new NewOrderNotification($fakeOrder));
+
+    return "Email and notification sent successfully!";
+});
